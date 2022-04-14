428ee1cf1885185b1c03313c9493a20d38f56428
ppettytheftt <jason.c.coffman23@gmail.com>
2022-04-14 02:04:13 -0500

    fix paper product controller

--- src/main/java/com/ppettytheftt/groceryapp/controller/PaperProductController.java
+++ src/main/java/com/ppettytheftt/groceryapp/controller/PaperProductController.java
@@ -1,6 +1,6 @@
 package com.ppettytheftt.groceryapp.controller;
 
-import com.ppettytheftt.groceryapp.model.FoodItem;
+import com.ppettytheftt.groceryapp.model.PaperProduct;
 import com.ppettytheftt.groceryapp.service.PaperProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,22 +15,22 @@ public class PaperProductController {
     private PaperProductService paperProductService;
 
     @RequestMapping(method = RequestMethod.GET, value = "grocery/paper-products")
-    public List<FoodItem> getAllPaperProducts() {
-        return paperProductService.getAllGroceries();
+    public List<PaperProduct> getAllPaperProducts() {
+        return paperProductService.getAllPaperProducts();
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/grocery/paper-products")
-    public void addPaperProduct(@RequestBody FoodItem groceryItem) {
-        paperProductService.addGroceryItem(groceryItem);
+    public void addPaperProduct(@RequestBody PaperProduct paperProduct) {
+        paperProductService.addPaperProduct(paperProduct);
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "/grocery/paper-products/{id}")
-    public void updatePaperProduct(@PathVariable Integer id, @RequestBody FoodItem newGroceryItem){
-        paperProductService.updateGrocery(id, newGroceryItem);
+    public void updatePaperProduct(@PathVariable Integer id, @RequestBody PaperProduct newPaperProduct){
+        paperProductService.updatePaperProduct(id, newPaperProduct);
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "/grocery/paper-products/{id}")
     public void deletePaperProduct(@PathVariable Integer id) {
-        paperProductService.deleteGroceryById(id);
+        paperProductService.deletePaperProductById(id);
     }
 }
